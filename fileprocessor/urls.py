91bbbdd8ea056cd69212bbd5ff96b2497ae2f8ea
Kash087 <kashish87@antiersolutions.com>
2024-11-21 16:27:13 +0530

    audio and image to text

--- fileprocessor/urls.py
+++ fileprocessor/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('doc-to-text/', views.doc_to_text, name='doc_to_text'),
     path('txt-to-text/', views.txt_to_text, name='txt_to_text'),
     path('pdf-to-text/', views.pdf_to_text, name='pdf_to_text'),
+    path('audio-to-text/',views.audio_to_text,name='audio_to_text'),
+    path('image-to-text/',views.image_to_text,name='image_to_image'),
 ]
 
--- fileprocessor/views.py
+++ fileprocessor/views.py
@@ -9,6 +9,25 @@ from pytube import YouTube
 from moviepy.editor import AudioFileClip
 from docx import Document
 from PyPDF2 import PdfReader
+import tempfile
+import whisper
+import base64
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key='api_key')
+os.environ['GOOGLE_API_KEY'] = api_key
+
+
+LLM = ChatGoogleGenerativeAI(model='gemini-1.5-flash',
+                             temperature=0.4,
+                             max_output_tokens=512)
+
 
 @csrf_exempt
 def doc_to_text(request):
@@ -66,3 +85,69 @@ def pdf_to_text(request):
         else:
             return JsonResponse({"error": "Invalid file format. Only .pdf allowed."}, status=400)
     return JsonResponse({"error": "Invalid request method or missing file."}, status=400)
+
+
+
+@csrf_exempt
+def audio_to_text(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        file = request.FILES['file']
+        if file.name.endswith('.mp3'):
+            try:
+                with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio_file:
+                    for chunk in file.chunks():
+                        temp_audio_file.write(chunk)
+                    temp_audio_path = temp_audio_file.name
+
+                model = whisper.load_model("base")
+                result = model.transcribe(temp_audio_path)
+                text = result['text']
+
+                return JsonResponse({"text": text}, status=200)
+            except Exception as e:
+                return JsonResponse({"error": str(e)}, status=500)
+
+        else:
+            return JsonResponse({"error": "Invalid file format. Only .pdf allowed."}, status=400)
+
+    return JsonResponse({"error": "Invalid request method or missing file."}, status=400)
+
+@csrf_exempt
+def image_to_text(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        file = request.FILES['file']
+        print(file.name)
+        if file.name[file.name.index("."):] in ['.jpg','.png','.jpeg']:
+            try:
+                image_data = file.read()
+                b64_encoded_data = base64.b64encode(image_data)
+                b64_encoded_string = b64_encoded_data.decode('utf-8')
+                prompt = ChatPromptTemplate.from_messages(
+                    [
+                        ("system", "Describe all the details in the provided image, be verbose."),
+                        (
+                            "user", [
+                                {
+                                    "type": "image_url",
+                                    "image_url": {"url": "data:image/jpeg;base64,{image_data}"}
+                                }
+                            ]
+                        ),
+                        ("user", "Describe the given image. Explain everything in details, be very verbose")
+                    ]
+                )
+                chain = prompt | LLM
+                text = chain.invoke({"image_data": b64_encoded_string})
+                return JsonResponse({"text": text.content}, status=200)
+            except Exception as e:
+                return JsonResponse({"error": str(e)}, status=500)
+
+        else:
+            return JsonResponse({"error": "Invalid file format. Only .pdf allowed."}, status=400)
+
+    return JsonResponse({"error": "Invalid request method or missing file."}, status=400)
+
+
+
+
+
